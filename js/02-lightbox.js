bd9d19434d3a0162b0a23784d52aeaece0b8ad05
Imperface <kovzhenko.ua@gmail.com>
2023-08-20 12:28:20 +0300

    task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,29 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
+const galleryContainerRef = document.querySelector("ul.gallery");
 
-console.log(galleryItems);
+const renderGallery = (gallery, container) => {
+  const galleryMarkup = gallery
+    .map(
+      (image) =>
+        `<li class="gallery__item">
+          <a class="gallery__link" href="${image.original}" >
+            <img class="gallery__image" src="${image.preview}" alt="${image.description}"/>
+          </a>
+        </li>`
+    )
+    .join("");
+  container.insertAdjacentHTML("beforeend", galleryMarkup);
+};
+
+renderGallery(galleryItems, galleryContainerRef);
+
+let gallery = new SimpleLightbox(".gallery a", {
+  sourceAttr: "href",
+  scrollZoom: false,
+  captions: true,
+  captionDelay: 250,
+  captionSelector: "img",
+  captionType: "attr",
+  captionsData: "alt",
+  captionPosition: "bottom",
+});
