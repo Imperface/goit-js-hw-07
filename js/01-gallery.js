2001ab1b54a036596eb50ac857d5fea3dd4fa974
Imperface <kovzhenko.ua@gmail.com>
2023-08-20 10:02:09 +0300

    1 task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,33 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryContainerRef = document.querySelector("ul.gallery");
 
-console.log(galleryItems);
+const renderGallery = (gallery, container) => {
+  const galleryMarkup = gallery
+    .map(
+      (image) =>
+        `<li class="gallery__item">
+          <a class="gallery__link">
+            <img class="gallery__image" src="${image.preview}" alt="${image.description}"/>
+          </a>
+        </li>`
+    )
+    .join("");
+  container.insertAdjacentHTML("beforeend", galleryMarkup);
+};
+
+const onImageClick = (e) => {
+  //check
+  if (e.currentTarget === e.target) {
+    return;
+  }
+  const imageRef = galleryItems.find((item) => item.preview === e.target.src);
+
+  const instance = basicLightbox.create(
+    `<img src="${imageRef.original}" alt="${imageRef.description}" />`
+  );
+  instance.show();
+};
+
+renderGallery(galleryItems, galleryContainerRef);
+galleryContainerRef.addEventListener("click", onImageClick);
